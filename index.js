8d7ae540fe314be45906ccecda6020ff0e7ea5b1
Chase <chaseit12@gmail.com>
2017-12-19 03:27:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,14 +6,23 @@ function takeANumber(katzDeliLine, person){
     return `Welcome, ${person}. You are number ${katzDeliLine.length} in line.`
 }
 
-function nowServing (katzDeliLine){
-
-    if (katzDeliLine.length == 0){   // IF NO ONE IS IN THE LINE
+function nowServing (katzDeliLine){  
+    // IF NO ONE IS IN THE LINE
+    if (katzDeliLine.length == 0){ 
         return 'There is nobody waiting to be served!'
     }
     else{
         let current_person = katzDeliLine[0]
         katzDeliLine.shift()
         return `Currently serving ${current_person}.`
+    }
+}
+
+function currentLine(katzDeliLine){
+    if (katzDeliLine.length == 0){ 
+        return 'The line is currently empty.'
+    }
+    else{
+            
     }
 }
